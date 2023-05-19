a7e2f6d2b0d0a0ef960e40414da41c88705cee79
ricardo <gq1090523745@163.com>
2023-05-19 18:36:08 +0800

    链表使用

---
+++ ListLink/UsingLink.h
@@ -0,0 +1,26 @@
+#ifndef USINGLINK_H
+#define USINGLINK_H
+#include<iostream>
+#include<vector>
+#include"ListLink.h"
+#include"ListLinkUS.h"
+using namespace std;
+void UsListLinkFormat();
+
+void UsListLinkFormat(){
+    ListLink<int>LA,LB,LC;
+    LA.CreateList();
+    LB.CreateList();
+    //LA.SortALL();
+    //LB.SortALL();
+    LA.PrintList();
+    LB.PrintList();
+    ListLinkUS<int>obj;
+    //obj.LinkList_Unionlist(LA,LB,LC);
+    obj.LAandLBfost(LA,LB,LC);
+    LC.PrintList();
+}
+
+
+
+#endif
