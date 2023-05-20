23afbd553e9f16741734e8c59f033e47cfa8d5dd
ricardo <gq1090523745@163.com>
2023-05-20 22:21:05 +0800

    双向链表的实例（约瑟夫环）

--- DoubleList/UsDouble.h
+++ DoubleList/UsDouble.h
@@ -7,10 +7,23 @@ void UsDoubleFormat();
 
 void UsDoubleFormat(){
     DoubleList<int>s;
-    int N[6]={1,5,3,6,7,8};
-    for(int i=0;i<6;i++)s.InsertFront(N[i]);
+    int N;
+    cin>>N;
+    for(int i=0;i<N;i++)s.InsertAfter(i+1);
     s.PrintList();
-
+    /*约瑟夫环问题
+    cout<<endl;
+    int k,m;
+    cin>>k;
+    s.ResetsPtr();
+    s.GetNode(k);
+    cin>>m;
+    while(!s.ListEmpty()){
+        s.GetNode(m);
+        cout<<s.Data()<<" ";
+        s.DeleteAt();
+    }
+    */
 }
 
 
