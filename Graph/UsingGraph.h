eda20891203c66f59f25f714fac45e76555e51b1
ricardo <gq1090523745@163.com>
2023-05-24 22:41:47 +0800

    无向图与有向图的使用

--- Graph/UsingGraph.h
+++ Graph/UsingGraph.h
@@ -2,7 +2,10 @@
 #define USINGGRAPH_H
 #include "..//utils//common.hpp"
 #include "GraphAdjacency//GraphAdjacencyMatrix.h"
+#include "GraphAdjacencyList//GraphAdjacencyList.h"
+#include "GraphAdjacencyList//GraphAdjacencyListpoint.h"
 void UsMatrixFormat();
+void UsGraphAdjacencyListFormat();
 
 void UsMatrixFormat(){
     vector<int>vertiecs={1,2,3,4,5};
@@ -18,8 +21,45 @@ void UsMatrixFormat(){
     obj.print();
     cout<<endl;
     obj.removeVertex(2);   
-    cout<<endl; 
-    
+    cout<<endl;   
+}
+void UsGraphAdjacencyListFormat(){
+    /*创建链表*/
+    vector<Vertex*>v=valsToVets(vector<int>{1,3,2,5,4});//将int类型转换成vector<Vertex*>类型
+    vector<vector<Vertex*>>edges={{v[0],v[1]},{v[0],v[3]},{v[1],v[2]},
+                                    {v[2],v[3]},{v[2],v[4]},{v[3],v[4]}};
+    GraphAdjacencyListpoint<int>graph(edges);
+    cout<<"\n后的临界链表"<<endl;
+    graph.print();
+    graph.addEdge(v[0],v[2]);
+    cout<<"\n添加边1-2后，图为:";
+    graph.print();
+
+    vector<Vertex*>vet=graph.graphBFS(graph,v[0]);
+    cout<<"遍历后的顺序为:"<<endl;
+    printVector(vetsToVals(vet));
+
+    vector<Vertex*>ves=graph.graphDFS(graph,v[0]);
+    cout<<"深度优先遍历后为:"<<endl;
+    printVector(vetsToVals(ves));
+
+    graph.removeEdge(v[0],v[1]);
+    cout<<"\n删除边1-3后，图为:";
+    graph.print();
+
+    Vertex*v5=new Vertex(6);
+    graph.addVertex(v5);
+    cout<<"\n添加顶点6后图为:";
+    graph.print();
+
+    graph.removeVertex(v[1]);
+    cout<<"\n删除顶点3后图为:";
+    graph.print();        
+
+
+    for(auto vet:v){
+        delete vet;
+    }                       
 }
 
 #endif
