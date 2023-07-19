8d8f73f05842fab291ab052eeebae02a21e2582e
ricardo <gq1090523745@163.com>
2023-07-19 19:21:04 +0800

    无向有权图（最小生成树）

--- Graph/WeightedGraph/UnDriectedWeightedGraph.h
+++ Graph/WeightedGraph/UnDriectedWeightedGraph.h
@@ -3,6 +3,12 @@
 #define UNDRIECTEDWEIGHTEDGRAPH_H
 #include"..//..//utils//common.hpp"
 #include"VertexPiont.h"
+
+//优先队列判断条件
+bool cmp(pair<VertexPoint*,int>&a,pair<VertexPoint*,int>&b){
+    return a.second>b.second;
+}
+
 template<typename T>
 class UnDriectedWeightedGraph
 {
@@ -34,7 +40,8 @@ public:
             throw out_of_range("不存在此结点");
         //添加边vet1，vet2
         adjList[v1].push_back(vecs);
-        adjList[vecs.first].push_back(<v1,vecs.second>);
+        pair<VertexPoint*,int>As(v1,vecs.second);
+        adjList[vecs.first].push_back(As);
     }
     //删除边
     void removeEdge(VertexPoint*v1,VertexPoint*v2){
@@ -64,8 +71,12 @@ public:
             const auto&key=adj.first;
             const auto&vec=adj.second;
             cout<<key->val<<": ";
-            printSforTwo(vetsTovalsl(vec));
+            for(auto&v:vec){
+                cout<<v.first->key<<" "<<v.second<<" | ";
+            }
+            cout<<endl;
         }
+        cout<<endl;
     }
 private:
     /*无向图的广度优先遍历*//*传入邻接链表对象和顶点起始点*/
@@ -80,7 +91,7 @@ private:
             que.pop();//队首顶点出队
             res.push_back(vet);//记录访问结点
             for(auto adjVet:graph.adjList[vet]){//遍历adjList[vet]顶点的每个数组元素
-                if(visited.count(adjVet))//如果已经遍历过，就跳过此顶点
+                if(visited.count(adjVet.first))//如果已经遍历过，就跳过此顶点
                 continue;
                 que.push(adjVet.first);//将此顶点入队
                 visited.emplace(adjVet.first);//记录此顶点
@@ -100,6 +111,64 @@ private:
             UnDriectedWeightedGraphDFS(graph,visited,res,adjMat.first);
         }
     }
+    struct cmp1{
+        bool operator()(pair<VertexPoint*,pair<VertexPoint*,int>>&a,pair<VertexPoint*,pair<VertexPoint*,int>>&b){
+            return a.second.second>b.second.second;
+        }
+    };
+public:
+    /*最小生成树*/
+    /*种一棵树，让树从一个节点增长到n个节点*/
+    /*初始的时候数只有一个节点，每轮循环找出一个节点和一条边，将其接在树上*/
+    /*保证：1.树是联通图
+            2.没有回路*/
+    /*算法循环次数等于节点的数量*/
+
+    /*选入第一个节点，从次结点开始遍历邻接节点，并将此节点放入哈希表中*/
+    /*取出节点中最小的一条边与其对应的节点加入树中，并用哈希表记录*/
+    vector<pair<VertexPoint*,VertexPoint*>>PrimMinimumTree(UnDriectedWeightedGraph&graph,VertexPoint*start){
+        //记录边
+        vector<pair<VertexPoint*,VertexPoint*>>Edge;
+        //优先级队列，用来存放边，每轮循环取出优先级最低的一条边
+        priority_queue<pair<VertexPoint*,pair<VertexPoint*,int>>,vector<pair<VertexPoint*,pair<VertexPoint*,int>>>,cmp1>que;
+        //保证每个节点只能被到达一次
+        unordered_set<VertexPoint*>set;
+        //用来遍历节点
+        queue<VertexPoint*>ques;
+        //初始节点入队
+        ques.push(start);
+        //初始节点不用被到达
+        set.emplace(start);
+        while(!ques.empty()){
+            VertexPoint*ptr=ques.front();
+            ques.pop();
+            //获取边
+            pair<VertexPoint*,pair<VertexPoint*,int>>temp;
+            //获取边的起始点
+            temp.first=ptr;
+            for(auto&Gra:graph.adjList[ptr]){//遍历当前顶点的所有邻接点
+                if(set.count(Gra.first))//如果当前邻接点已经被到达过，就不用再遍历了
+                continue;
+                temp.second.first=Gra.first;
+                temp.second.second=Gra.second;
+                que.push(temp);
+            }
+            //如果边数已经是节点总数-1，那么证明找完了，退出即可
+            if(Edge.size()==graph.adjList.size()-1)
+            break;
+            temp=que.top();
+            while(set.count(temp.second.first)){
+                que.pop();
+                temp=que.top();
+            }
+            que.pop();
+            pair<VertexPoint*,VertexPoint*>tep(temp.first,temp.second.first);
+            Edge.push_back(tep);
+            ques.push(tep.second);
+            set.emplace(tep.second);
+        }
+        return Edge;
+    }
 public:
     /*无向有权图的广度优先遍历*/
     vector<vector<int>>UnDriectedWeightedGraphBFSIn(UnDriectedWeightedGraph&graph,VertexPoint*start){
@@ -112,7 +181,12 @@ public:
         UnDriectedWeightedGraphDFS(graph,visited,res,start);
         return vetsTovalsl(res);
     }
-    
+    void PrimMinimumTreeIn(UnDriectedWeightedGraph&graph,VertexPoint*start){
+        for(auto&res:PrimMinimumTree(graph,start)){
+            cout<<"("<<res.first->key<<" , "<<res.second->key<<")"<<endl;
+        }
+        cout<<endl;
+    }
 };
 
 
